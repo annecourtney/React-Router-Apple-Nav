03a36ae07dd94f0975b312d578073ae9d827cd99
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-08 17:57:53 -0500

    added subnav.js file to hold the new arrays of data for the sublinks. Just unsure how to connect them now to the regular nav links.

--- apple-nav/src/Components/mac.js
+++ apple-nav/src/Components/mac.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+const myMac = ['MacBook', 'MacBook Air', 'MacBook Pro', 'iMac', 'iMac Pro', 'Mac Pro', 'Mac mini', 'Accessories', 'High Sierra', 'Compare']
+
 const Mac = () => {
     return (
+        <div>
         <p>Mac</p>
+        </div>
     )
 }
 
---
+++ apple-nav/src/Components/subnav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Apple, Checkout, Ipad, Iphone, Mac, Music, Search, Support, Tv, Watch } from './Components/index';
+
+
+const myMac = ['MacBook', 'MacBook Air', 'MacBook Pro', 'iMac', 'iMac Pro', 'Mac Pro', 'Mac mini', 'Accessories', 'High Sierra', 'Compare']
+const myIpad = ['iPad Pro', 'iPad', 'iPad mini 4', 'iOS 11', 'Accessories', 'Compare']
+const myIphone = ['iPhone X', 'iPhone 8', 'iPhone 7', 'iPhone 6', 'iPhone SE', 'iOS 11', 'Accessories', 'Compare']
+const myWatch = ['Apple Watch Series 3', 'Apple Watch Nike +', 'Apple Watch Hermes', 'Apple Watch Edition', 'Apple Watch Series 1', 'watchOS', 'Bands', 'Accessories', 'Compare']
+const myTv = ['Apple TV 4k', 'Apple TV', 'TV App', 'Accessories', 'Compare']
+const myMusic = ['Apple Music', 'iTunes', 'HomePod', 'iPod touch', 'Music Accessories', 'Gift Cards']
+
+
